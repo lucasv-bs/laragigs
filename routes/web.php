0b832b795454353f9a1c9c51a1a1c1ded50cd9bd
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-07-10 04:55:13 -0300

    Add the page to store a new Listing

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,12 @@ use Illuminate\Support\Facades\Route;
 // All Listings
 Route::get('/', [ListingController::class, 'index']);
 
+
+// Show create form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+// Store Listing data
+Route::post('/listings', [ListingController::class, 'store']);
+
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
